91137311f10b8bb654173956045cb565d98d5a46
Andrey Voznyuk <voznyk.an@yandex.ru>
2020-06-08 21:52:36 +0300

    Project structure review. Build script.
    - root directory CMakeLists.txt added.
    - aloe dir which will contain all of the engine code was renamed to
    "engine".
    - engine and demo folders are included in the root CMakeLists.txt as
      subdirs.
    - static linking for engine library.
    - build.sh script for cmake -> make -> run process.

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+check_success() 
+{
+    if [[ $status != 0 ]];
+    then
+        echo "Process finished with error code $status"
+        exit $status
+    fi
+}
+
+echo "Running CMake"
+cmake -S . -B build
+status=$?
+check_success
+
+echo "Running Make"
+cd build
+make
+status=$?
+cd -
+check_success
+
+echo "Your build is inside 'build/demo/bin'"
+echo "Press R to run or other key to exit..."
+read -n 1 input <&1
+if [[ $input = r ]];
+then
+    echo ""
+    echo "Running..."
+    ./build/demo/bin/demo
+fi
+
